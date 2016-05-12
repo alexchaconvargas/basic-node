8bc8b06025d9569568fbe0e19331df7761c76a94
Alejandro Chacón <alexchaconvargas@gmail.com>
2016-05-12 11:37:00 +0200

    Delete helloworld.spec.js

--- test/helloworld.spec.js
+++
@@ -1,8 +0,0 @@
-var assert = require('chai').assert;
-var helloworld = require('../app/helloworld');
-
-describe('First suite', function () {
-	it('should show hello world', function () {
-      assert.equal(helloworld(), 'Hello world');
-    });
-});
